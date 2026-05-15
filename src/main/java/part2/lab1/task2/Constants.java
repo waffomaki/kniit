852234ece86d9c18e6d752b7ecbc37c0bcf90c66
waffomaki <waffomaki@gmail.com>
2026-05-15 12:16:46 +0400

    fixed constants

--- src/main/java/part2/lab1/task2/Constants.java
+++ src/main/java/part2/lab1/task2/Constants.java
@@ -3,7 +3,7 @@ package part2.lab1.task2;
 public final class Constants {
     public static final int TOTAL_DISHES = 6;
     public static final int MAX_TRAY_SIZE = 3;
-    public static final int COOK_DELAY_MS = 400;
+    public static final int COOK_DELAY_MS = 200;
     public static final int WAITER_DELAY_MS = 600;
 
     private Constants() {
--- src/main/java/part2/lab1/task5/Main.java
+++ src/main/java/part2/lab1/task5/Main.java
@@ -8,11 +8,11 @@ import part2.lab1.task5.workers.WarehouseWorker;
 
 public class Main {
     public static void main(String[] args) {
-        Conveyor conveyor = Conveyor.getInstance();
-
         Thread stamper = new Thread(new StampingWorker(Constants.TOTAL_PARTS));
         Thread assembler = new Thread(new AssemblyWorker());
+        assembler.setDaemon(true);
         Thread qc = new Thread(new QualityControlWorker());
+        qc.setDaemon(true);
         Thread warehouse = new Thread(new WarehouseWorker(Constants.TOTAL_PARTS));
 
         warehouse.start();
